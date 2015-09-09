aeba97113a96bffdda1497b80c16403460bb39f6
welder496 <welder496@gmail.com>
2015-09-09 11:14:37 -0300

    Modificação de routerPerfil com adição de rota de atualização

--- routes/routerPerfil.js
+++ routes/routerPerfil.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var routerPerfil = express.Router();
+var routerPerfil = express.Router({mergeParams: true});
 var mongoose = require('mongoose');
 var perfil = require('../model/perfis');
 
@@ -7,7 +7,7 @@ routerPerfil.route('/all')
 
       .get(function(req,res){
              perfil = mongoose.model('Perfil');
-             perfil.find().sort({'descricao': -1}).exec(function(err, perfil) {
+             perfil.find().sort({'descricao': 1}).exec(function(err, perfil) {
                     if (err)
                           res.send(err);
                     if (perfil != null) {
@@ -30,7 +30,7 @@ routerPerfil.route('/new')
                    var Perfil = new perfil();
                    Perfil.descricao = descricao;
 
-                   models.perfil.findOne({descricao: Perfil.descricao}, function(err, perfil){
+                   perfil.findOne({descricao: Perfil.descricao}, function(err, perfil){
                         if (perfil != null) {
                               res.json({message: "Perfil foi encontrado!!"});
                         } else {
@@ -45,5 +45,70 @@ routerPerfil.route('/new')
                 res.json({message:"Não foi possível cadastrar a descrição de Perfil!!"});
              }
       });
+
+routerPerfil.route('/:descricao')
+
+      .delete(function(req,res){
+             perfil = mongoose.model('Perfil');
+             var descricao = req.params.descricao;
+             if (descricao != "") {
+                    perfil.remove({descricao: descricao}, function(err,perfil){
+                        if (err)
+                              res.send(err);
+                        if (perfil != null)
+                            res.json({message: "Perfil excluído com sucesso!!"});
+                    });
+             } else {
+                   res.json({message: "Não foi possível apagar a descrição de Perfil!!"});
+            }
+      });
+
+routerPerfil.route('/:id')
+
+      .delete(function(req,res){
+             perfil = mongoose.model('Perfil');
+             var id = req.params.id;
+             if (id != "") {
+                    perfil.remove({_id: id}, function(err,perfil){
+                        if (err)
+                              res.send(err);
+                        if (perfil != null) {
+                            res.json({message: "Perfil excluído com sucesso!!"});
+                        } else {
+                            res.json({message: "Perfil não foi encontrado!!"});
+                        }
+                    });
+             } else {
+                   res.json({message: "Não foi possível apagar a descrição de Perfil!!"});
+            }
+      });
+
+
+routerPerfil.route('/:id/:descricao')
+
+      .put(function(req,res){
+             perfil = mongoose.model('Perfil');
+             var id = req.params.id;
+             var descricao = req.params.descricao;
+             if (id != "" && descricao!="") {
+                    perfil.findOne({_id: id}, function(err,perfil){
+                          if (err)
+                               res.send(err);
+                          if (perfil != null) {
+                                perfil.descricao = descricao;
+                                perfil.save(function(err){
+                                      if (err)
+                                             res.send(err);
+                                       res.json({message: "Perfil alterado com sucesso!!"});
+                                });
+                          } else {
+                                res.json({message: "Perfil não foi encontrado!!"});
+                          }
+                   });
+             } else {
+                   res.json({message: "Não foi possível alterar o Perfil!!"});
+             }
+      });
+
 
 module.exports = routerPerfil;
